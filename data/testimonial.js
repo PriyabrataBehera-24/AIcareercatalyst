316fce7f064ed7802707fda6f9cf0f50c0ac2833
Priyabrata Behera <priyabratabehera@Priyabratas-Air.lan>
2025-02-01 18:38:08 +0530

    preparing for deployment

---
+++ data/testimonial.js
@@ -0,0 +1,26 @@
+export const testimonial = [
+  {
+    quote:
+     "This AI career coach gave me clear direction. I landed a high-paying role faster than expected!",
+    author: "Ankita Jain",
+    image: "https://randomuser.me/api/portraits/women/75.jpg",
+    role: "Software Engineer",
+    company: "InnoTech Solutions",
+  },
+  {
+    quote:
+      "With AI-driven resume optimization, my profile stood out instantly. Landed multiple job offers!",
+    author: "Rahul Mehta",
+    image: "https://randomuser.me/api/portraits/men/75.jpg",
+    role: "Cybersecurity Analyst",
+    company: "SecureNet Pvt Ltd",
+  },
+  {
+    quote:
+      "The personalized career insights helped me transition into tech smoothly. Best decision ever!",
+    author: "Sophia Dsouza",
+    image: "https://randomuser.me/api/portraits/women/74.jpg",
+    role: "UX Designer",
+    company: "Creative Labs",
+  },
+];
